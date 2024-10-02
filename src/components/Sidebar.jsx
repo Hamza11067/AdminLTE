c85c42ff1036bf1b1046c482330dbd2312af8707
Hamza11067 <hamzakhalid1067@gmail.com>
2024-10-02 11:07:59 +0500

    working on sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -76,7 +76,7 @@ const Sidebar = () => {
         {/* widgets */}
         <div className="row px-2 py-1 bg-dark">
           <div
-            className="px-3 py-2 rounded bg-dark-subtle text-white d-flex justify-content-between align-items-center"
+            className="px-3 py-2 rounded bg-dark-body text-white d-flex justify-content-between align-items-center"
             href="#"
             role="button"
           >
@@ -93,15 +93,14 @@ const Sidebar = () => {
           </div>
         </div>
         {/* layout options */}
-
         <div className="row px-2 py-1 bg-dark">
           <a
-            className="nav-link dropdown-toggle px-3 py-2 rounded bg-dark-subtle text-white d-flex justify-content-between align-items-center collapsed"
+            className="nav-link dropdown-toggle px-3 py-2 rounded bg-dark-body text-white d-flex justify-content-between align-items-center collapsed"
             href="#"
-            id="dashboardDropdown2"
+            id="layoutDropdown"
             role="button"
             data-bs-toggle="collapse"
-            data-bs-target="#dashboardMenu2"
+            data-bs-target="#layoutMenu"
             aria-expanded="false"
           >
             <div className="d-flex align-items-center">
@@ -109,11 +108,14 @@ const Sidebar = () => {
               <span className="ms-2 fw-semibold">Layout Options</span>
             </div>
           </a>
-          <ul className="list-group p-0 mt-2 collapse" id="dashboardMenu2">
-            <li className="list-group-item">
+          <ul
+            className="list-group list-group-flush p-0 mt-2 collapse"
+            id="layoutMenu"
+          >
+            <li className="list-group-item bg-transparent text-white border-0">
               <div className="form-check form-check-inline">
                 <input
-                  className="form-check-input"
+                  className="form-check-input bg-transparent"
                   type="radio"
                   name="layoutRadioOptions"
                   id="inlineRadio1"
@@ -124,10 +126,10 @@ const Sidebar = () => {
                 </label>
               </div>
             </li>
-            <li className="list-group-item">
+            <li className="list-group-item bg-transparent text-white border-0">
               <div className="form-check form-check-inline">
                 <input
-                  className="form-check-input"
+                  className="form-check-input bg-transparent"
                   type="radio"
                   name="layoutRadioOptions"
                   id="inlineRadio2"
@@ -138,10 +140,10 @@ const Sidebar = () => {
                 </label>
               </div>
             </li>
-            <li className="list-group-item">
+            <li className="list-group-item bg-transparent text-white">
               <div className="form-check form-check-inline">
                 <input
-                  className="form-check-input"
+                  className="form-check-input bg-transparent"
                   type="radio"
                   name="layoutRadioOptions"
                   id="inlineRadio3"
@@ -157,7 +159,7 @@ const Sidebar = () => {
         {/* charts */}
         <div className="row px-2 py-1 bg-dark">
           <a
-            className="nav-link dropdown-toggle px-3 py-2 rounded bg-dark-subtle text-white d-flex justify-content-between align-items-center collapsed"
+            className="nav-link dropdown-toggle px-3 py-2 rounded bg-dark-body text-white d-flex justify-content-between align-items-center collapsed"
             href="#"
             id="chartDropdown"
             role="button"
@@ -170,60 +172,49 @@ const Sidebar = () => {
               <span className="ms-2 fw-semibold">Charts</span>
             </div>
           </a>
-          <ul className="list-group p-0 mt-2 collapse" id="chartMenu">
-            <li className="list-group-item">
+          <ul
+            className="list-group list-group-flush p-0 mt-2 collapse"
+            id="chartMenu"
+          >
+            <li className="list-group-item bg-transparent text-white border-0">
               <div className="form-check form-check-inline">
                 <input
-                  className="form-check-input"
+                  className="form-check-input bg-transparent"
                   type="radio"
-                  name="chartRadioOptions"
+                  name="layoutRadioOptions"
                   id="inlineRadio1"
                   value="option1"
                 />
                 <label className="form-check-label" htmlFor="inlineRadio1">
-                  ChartJS
+                  Top Navigation
                 </label>
               </div>
             </li>
-            <li className="list-group-item">
+            <li className="list-group-item bg-transparent text-white border-0">
               <div className="form-check form-check-inline">
                 <input
-                  className="form-check-input"
+                  className="form-check-input bg-transparent"
                   type="radio"
-                  name="chartRadioOptions"
+                  name="layoutRadioOptions"
                   id="inlineRadio2"
                   value="option2"
                 />
                 <label className="form-check-label" htmlFor="inlineRadio2">
-                  Flot
+                  Fixed Sidebar
                 </label>
               </div>
             </li>
-            <li className="list-group-item">
+            <li className="list-group-item bg-transparent text-white">
               <div className="form-check form-check-inline">
                 <input
-                  className="form-check-input"
+                  className="form-check-input bg-transparent"
                   type="radio"
-                  name="chartRadioOptions"
+                  name="layoutRadioOptions"
                   id="inlineRadio3"
                   value="option3"
                 />
                 <label className="form-check-label m-0" htmlFor="inlineRadio3">
-                  Inline
-                </label>
-              </div>
-            </li>
-            <li className="list-group-item">
-              <div className="form-check form-check-inline">
-                <input
-                  className="form-check-input"
-                  type="radio"
-                  name="chartRadioOptions"
-                  id="inlineRadio3"
-                  value="option3"
-                />
-                <label className="form-check-label m-0" htmlFor="inlineRadio3">
-                  uPlot
+                  Boxed
                 </label>
               </div>
             </li>
@@ -232,7 +223,7 @@ const Sidebar = () => {
         {/* UI elements */}
         <div className="row px-2 py-1 bg-dark">
           <a
-            className="nav-link dropdown-toggle px-3 py-2 rounded bg-dark-subtle text-white d-flex justify-content-between align-items-center collapsed"
+            className="nav-link dropdown-toggle px-3 py-2 rounded bg-dark-body text-white d-flex justify-content-between align-items-center collapsed"
             href="#"
             id="chartDropdown"
             role="button"
@@ -245,60 +236,49 @@ const Sidebar = () => {
               <span className="ms-2 fw-semibold">UI Elements</span>
             </div>
           </a>
-          <ul className="list-group p-0 mt-2 collapse" id="uiMenu">
-            <li className="list-group-item">
+          <ul
+            className="list-group list-group-flush p-0 mt-2 collapse"
+            id="uiMenu"
+          >
+            <li className="list-group-item bg-transparent text-white border-0">
               <div className="form-check form-check-inline">
                 <input
-                  className="form-check-input"
+                  className="form-check-input bg-transparent"
                   type="radio"
-                  name="uiRadioOptions"
+                  name="layoutRadioOptions"
                   id="inlineRadio1"
                   value="option1"
                 />
                 <label className="form-check-label" htmlFor="inlineRadio1">
-                  General
+                  Top Navigation
                 </label>
               </div>
             </li>
-            <li className="list-group-item">
+            <li className="list-group-item bg-transparent text-white border-0">
               <div className="form-check form-check-inline">
                 <input
-                  className="form-check-input"
+                  className="form-check-input bg-transparent"
                   type="radio"
-                  name="uiRadioOptions"
+                  name="layoutRadioOptions"
                   id="inlineRadio2"
                   value="option2"
                 />
                 <label className="form-check-label" htmlFor="inlineRadio2">
-                  Icons
+                  Fixed Sidebar
                 </label>
               </div>
             </li>
-            <li className="list-group-item">
+            <li className="list-group-item bg-transparent text-white">
               <div className="form-check form-check-inline">
                 <input
-                  className="form-check-input"
+                  className="form-check-input bg-transparent"
                   type="radio"
-                  name="chartRadioOptions"
+                  name="layoutRadioOptions"
                   id="inlineRadio3"
                   value="option3"
                 />
                 <label className="form-check-label m-0" htmlFor="inlineRadio3">
-                  Buttons
-                </label>
-              </div>
-            </li>
-            <li className="list-group-item">
-              <div className="form-check form-check-inline">
-                <input
-                  className="form-check-input"
-                  type="radio"
-                  name="chartRadioOptions"
-                  id="inlineRadio3"
-                  value="option3"
-                />
-                <label className="form-check-label m-0" htmlFor="inlineRadio3">
-                  Sliders
+                  Boxed
                 </label>
               </div>
             </li>
@@ -307,7 +287,7 @@ const Sidebar = () => {
         {/* forms */}
         <div className="row px-2 py-1 bg-dark">
           <a
-            className="nav-link dropdown-toggle px-3 py-2 rounded bg-dark-subtle text-white d-flex justify-content-between align-items-center collapsed"
+            className="nav-link dropdown-toggle px-3 py-2 rounded bg-dark-body text-white d-flex justify-content-between align-items-center collapsed"
             href="#"
             id="chartDropdown"
             role="button"
@@ -320,60 +300,49 @@ const Sidebar = () => {
               <span className="ms-2 fw-semibold">Forms</span>
             </div>
           </a>
-          <ul className="list-group p-0 mt-2 collapse" id="formMenu">
-            <li className="list-group-item">
+          <ul
+            className="list-group list-group-flush p-0 mt-2 collapse"
+            id="formMenu"
+          >
+            <li className="list-group-item bg-transparent text-white border-0">
               <div className="form-check form-check-inline">
                 <input
-                  className="form-check-input"
+                  className="form-check-input bg-transparent"
                   type="radio"
-                  name="formRadioOptions"
+                  name="layoutRadioOptions"
                   id="inlineRadio1"
                   value="option1"
                 />
                 <label className="form-check-label" htmlFor="inlineRadio1">
-                  General
+                  Top Navigation
                 </label>
               </div>
             </li>
-            <li className="list-group-item">
+            <li className="list-group-item bg-transparent text-white border-0">
               <div className="form-check form-check-inline">
                 <input
-                  className="form-check-input"
+                  className="form-check-input bg-transparent"
                   type="radio"
-                  name="formRadioOptions"
+                  name="layoutRadioOptions"
                   id="inlineRadio2"
                   value="option2"
                 />
                 <label className="form-check-label" htmlFor="inlineRadio2">
-                  Icons
+                  Fixed Sidebar
                 </label>
               </div>
             </li>
-            <li className="list-group-item">
+            <li className="list-group-item bg-transparent text-white">
               <div className="form-check form-check-inline">
                 <input
-                  className="form-check-input"
+                  className="form-check-input bg-transparent"
                   type="radio"
-                  name="formRadioOptions"
+                  name="layoutRadioOptions"
                   id="inlineRadio3"
                   value="option3"
                 />
                 <label className="form-check-label m-0" htmlFor="inlineRadio3">
-                  Buttons
-                </label>
-              </div>
-            </li>
-            <li className="list-group-item">
-              <div className="form-check form-check-inline">
-                <input
-                  className="form-check-input"
-                  type="radio"
-                  name="formRadioOptions"
-                  id="inlineRadio3"
-                  value="option3"
-                />
-                <label className="form-check-label m-0" htmlFor="inlineRadio3">
-                  Sliders
+                  Boxed
                 </label>
               </div>
             </li>
@@ -382,7 +351,7 @@ const Sidebar = () => {
         {/* tables */}
         <div className="row px-2 py-1 bg-dark">
           <a
-            className="nav-link dropdown-toggle px-3 py-2 rounded bg-dark-subtle text-white d-flex justify-content-between align-items-center collapsed"
+            className="nav-link dropdown-toggle px-3 py-2 rounded bg-dark-body text-white d-flex justify-content-between align-items-center collapsed"
             href="#"
             id="chartDropdown"
             role="button"
@@ -395,46 +364,49 @@ const Sidebar = () => {
               <span className="ms-2 fw-semibold">Tables</span>
             </div>
           </a>
-          <ul className="list-group p-0 mt-2 collapse" id="tableMenu">
-            <li className="list-group-item">
+          <ul
+            className="list-group list-group-flush p-0 mt-2 collapse"
+            id="tableMenu"
+          >
+            <li className="list-group-item bg-transparent text-white border-0">
               <div className="form-check form-check-inline">
                 <input
-                  className="form-check-input"
+                  className="form-check-input bg-transparent"
                   type="radio"
-                  name="tableRadioOptions"
+                  name="layoutRadioOptions"
                   id="inlineRadio1"
                   value="option1"
                 />
                 <label className="form-check-label" htmlFor="inlineRadio1">
-                  SimpleTables
+                  Top Navigation
                 </label>
               </div>
             </li>
-            <li className="list-group-item">
+            <li className="list-group-item bg-transparent text-white border-0">
               <div className="form-check form-check-inline">
                 <input
-                  className="form-check-input"
+                  className="form-check-input bg-transparent"
                   type="radio"
-                  name="tableRadioOptions"
+                  name="layoutRadioOptions"
                   id="inlineRadio2"
                   value="option2"
                 />
                 <label className="form-check-label" htmlFor="inlineRadio2">
-                  DataTables
+                  Fixed Sidebar
                 </label>
               </div>
             </li>
-            <li className="list-group-item">
+            <li className="list-group-item bg-transparent text-white">
               <div className="form-check form-check-inline">
                 <input
-                  className="form-check-input"
+                  className="form-check-input bg-transparent"
                   type="radio"
-                  name="tableRadioOptions"
+                  name="layoutRadioOptions"
                   id="inlineRadio3"
                   value="option3"
                 />
                 <label className="form-check-label m-0" htmlFor="inlineRadio3">
-                  jsGrid
+                  Boxed
                 </label>
               </div>
             </li>
@@ -449,7 +421,7 @@ const Sidebar = () => {
         {/* calender */}
         <div className="row px-2 py-1 bg-dark">
           <a
-            className="nav-link px-3 py-2 rounded bg-dark-subtle text-white d-flex justify-content-between align-items-center"
+            className="nav-link px-3 py-2 rounded bg-dark-body text-white d-flex justify-content-between align-items-center"
             href="#"
             role="button"
           >
